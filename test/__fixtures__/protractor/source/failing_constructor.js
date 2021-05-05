9677e32ccf419a528f6c635221825b86f87a091d
christian-bromann <mail@christian-bromann.com>
2021-05-06 01:12:06 +0200

    revert test change

--- test/__fixtures__/protractor/source/failing_constructor.js
+++ test/__fixtures__/protractor/source/failing_constructor.js
@@ -1,6 +1,6 @@
 class FriendsPage {
     constructor() {
-        this.binding = element(by.css('h2.ng-binding'))
+        this.binding = $('h2.ng-binding')
         this.pageLoaded = this.isClickable(this.binding);
     }
 }
