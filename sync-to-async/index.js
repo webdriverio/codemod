7f7c02751d674acc38d77a4e0ab2cc4763033859
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2021-08-18 16:43:49 +0200

    feat: transforms $ and $$ to async

--- sync-to-async/index.js
+++ sync-to-async/index.js
@@ -1,5 +1,13 @@
 const compilers = require('../common/compilers')
 
+const wrapWithAsync = path => {
+    if (path.parent.value.type !== "AwaitExpression") return {
+        type: 'AwaitExpression',
+        argument: path.value
+    }
+    return path.value
+};
+
 module.exports = function transformer(file, api, opts) {
     const j = api.jscodeshift;
     const root = j(file.source);
@@ -8,13 +16,23 @@ module.exports = function transformer(file, api, opts) {
     /**
      * transforms all it() parameter to async
      */
-    root.find(j.CallExpression, { callee: { name: 'it'} }).replaceWith(
+    root.find(j.CallExpression, { callee: { name: 'it' } }).replaceWith(
         path => {
-          if (path.value.arguments[1] && path.value.arguments[1].type === "ArrowFunctionExpression") {
-            path.value.arguments[1].async = true
-          }
-          return path.value
+            if (path.value.arguments[1] && path.value.arguments[1].type === "ArrowFunctionExpression") {
+                path.value.arguments[1].async = true
+            }
+            return path.value
         }
+    )
+
+    /**
+     * transforms $ and $$ calls to async
+     */
+    root.find(j.CallExpression, { callee: { name: '$' } }).replaceWith(
+        wrapWithAsync
+    )
+    root.find(j.CallExpression, { callee: { name: '$$' } }).replaceWith(
+        wrapWithAsync
     )
 
     compilers.update(j, root, autoCompileOpts, opts)
